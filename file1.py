bc7ff93ff1bbd5c2ed080748e154ceba3a144446
valtyrmar <valtyrmm@gmail.com>
2024-06-09 11:42:58 +0200

    and this

--- file1.py
+++ file1.py
@@ -1,1 +1,3 @@
 print("Han is a genius")
+#Forstjóri Sjúkratrygginga Íslands segir að bíða verði eftir því að fleiri þyngdarstjórnunarlyf komi á markað, áður en hægt verður að rýmka skilyrði fyrir greiðslyuþátttöku. Sem stendur sé samfélagslegur kostnaður of mikill. Fólk hefur í auknum mæli tekið sjálft á sig kostnað lyfjanna.
+#Sprenging hefur orðið á notkun þyngdarstjórnunarlyfja, líkt og 
