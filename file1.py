b3c7912167bf770af3f84b7a2353647f0c728725
valtyrmar <valtyrmm@gmail.com>
2024-06-17 13:40:35 +0200

    ee

--- file1.py
+++ file1.py
@@ -1,14 +1,1 @@
-print("Han is a genius")
-#Forstjóri Sjúkratrygginga Íslands segir að bíða verði eftir því að fleiri þyngdarstjórnunarlyf komi á markað, áður en hægt verður að rýmka skilyrði fyrir greiðslyuþátttöku. Sem stendur sé samfélagslegur kostnaður of mikill. Fólk hefur í auknum mæli tekið sjálft á sig kostnað lyfjanna.
-#Sprenging hefur orðið á notkun þyngdarstjórnunarlyfja, líkt og 
-
-print('maks is not funny')
-
-
-print("let's try this")
-virkar þetta
-
-helloooo
-
-
-print("Hello, World!")
+print("poland")
